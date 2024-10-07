c4d157a2b3901e2adf1313823c3f5d705c7c284a
0EFB6 <changws12345@gmail.com>
2024-10-07 07:49:54 +0000

    Changes to be committed: 	new file:   client/.eslintrc.cjs 	new file:   client/.gitignore 	new file:   client/.prettierrc.json 	new file:   client/.vscode/extensions.json 	new file:   client/README.md 	new file:   client/env.d.ts 	new file:   client/index.html 	new file:   client/package.json 	new file:   client/pnpm-lock.yaml 	new file:   client/public/favicon.ico 	new file:   client/src/App.vue 	new file:   client/src/components/TestComponent.vue 	new file:   client/src/main.ts 	new file:   client/src/router/index.ts 	new file:   client/src/views/HomeView.vue 	new file:   client/tsconfig.app.json 	new file:   client/tsconfig.json 	new file:   client/tsconfig.node.json 	new file:   client/vite.config.ts

---
+++ client/src/components/TestComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps<{username: string; age: number}>();
+const ages = props.age + 1;
+</script>
+
+<template>
+    <h1>This is test component.</h1>
+    User: {{ props.username }}
+    <br>
+    Age: {{  ages }}
+
+    <slot></slot>
+    <p><slot name="description"></slot></p>
+</template>
+
+<style scoped>
+    h1 {
+        color: yellow;
+    }
+    p {
+        color: green;
+    }
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import TestComponent from '@/components/TestComponent.vue';
+import { ref, type Ref, computed } from 'vue';
+
+const username: string = 'Alex';
+const userRole: Ref<string> = ref('admin');
+const fruits: string[] = ['Apple', 'Banana', 'Orange', 'Grapes'];
+
+const inputValue = ref('Starting Value');
+
+const htmlContent = computed(() => {
+  return `<p style="color:red;"> ${inputValue.value}</p>`;
+});
+
+const counter = ref(0);
+const incrementCounter = () => {
+  counter.value++
+};
+</script>
+
+<template>
+  <main>
+    Hello world!
+  </main>
+  <h1>This is main header</h1>
+  {{ username }}
+  <TestComponent username="username" v-bind:age="22">
+    <p>This is a description</p>
+    <template v-slot:description>This is another description</template>
+  </TestComponent>
+  <p v-if="userRole === 'admin'">You are an Admin</p>
+  <p v-else-if="userRole === 'user'">You are a regular User</p>
+  <p v-else>You are a guest</p>
+
+  <li v-for="(fruit,index) in fruits" :key="index">{{ index + 1 }}. {{  fruit }}</li>
+
+  <!-- <input
+  v-bind:value="inputValue"
+  v-on:input="(event) =>(inputValue = (event.target as HTMLInputElement).value)"
+  /> -->
+
+  <input v-model="inputValue"/>
+  <div v-html="htmlContent"></div>
+  <h3 v-on:click="incrementCounter">You have click on me {{ counter }} times.</h3>
+</template>
